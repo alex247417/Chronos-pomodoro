d88ff64cc56efa13b1375e05a5abb49df62d59fe
Alecsandro Silva <alex247417@hotmail.com>
2026-06-21 08:40:45 -0300

    refactor: reorganiza Conteiner e Heading em pastas e adiciona componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Conteiner } from './components/conteiner';
+import { Conteiner } from './components/Conteiner';
 import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+
 
 
 export function App() {
   return (
     <>
+    <Conteiner>
+      <Logo />
+    </Conteiner>
      <Conteiner>
        <Heading>Logo</Heading>
      </Conteiner>
