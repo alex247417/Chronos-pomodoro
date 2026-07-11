5ec5958bd07c6e25f89dff5fa865d909e7ebcdaa
Alecsandro Silva <alex247417@hotmail.com>
2026-07-11 19:51:40 -0300

    feat(ui): add DefaultInput component and integrate it into App
    Include the new DefaultInput component in the components folder, import it into App.tsx, and apply related style updates in global.css.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Conteiner } from './components/Conteiner';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+
 
 
 
@@ -18,6 +20,28 @@ export function App() {
      </Conteiner>
     <Conteiner>
       <CountDown />
+    </Conteiner>
+    <Conteiner>
+      <>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput labelText='Task'id='meuInput' type='text' /> 
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </>
     </Conteiner>
     </>
   );
